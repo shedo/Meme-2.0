851519a555d46f6e7a85c33ff5d7043e20936b16
Ivan Zandona <i.zandona@reply.it>
2020-09-27 16:47:01 +0200

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Ivan Zandonà on 27/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
